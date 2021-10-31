8355df1f912c53b6f969118fb4a4e2823f3e76fa
Sugar <sugarypolecat@gmail.com>
2021-10-31 15:57:24 +0000

    init i guess

---
+++ Cwiczenia AGH/Cwiczenia 2/Zadanie 1/Osoba.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1
+{
+    enum Plcie
+    {
+        K, M
+    }
+    class Osoba
+    {
+        private string imie;
+        public string Nazwisko { get; set; }
+        private DateTime dataUrodzenia;
+        private string PESEL;
+        private Plcie plec;
+
+        public string Imie
+        {
+            get { return imie; }
+            set { imie = value; }
+        }
+        public DateTime DataUrodzenia { get => dataUrodzenia; set => dataUrodzenia = value; }
+        public string Pesel
+        {
+            get { return PESEL; }
+            set { PESEL = value; }
+        }
+        Osoba()
+        {
+
+        }
+        Osoba(string imie, string nazwisko)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+        }
+        Osoba(string imie, string nazwisko, string data_urodzenia, string pesel, Plcie plec)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            DateTime.TryParseExact(data_urodzenia, new[] {"yyyy-MM-dd", "yyyy/MM/dd", "MM/dd/yy", "dd-MMM-yy"}, null, DateTimeStyles.None, out dataUrodzenia);
+            Pesel = pesel;
+            this.plec = plec;
+        }
+        /*override string ToString()
+        {
+
+        }
+        int Wiek() { }
+        */
+    }
+}
---
+++ Cwiczenia AGH/Cwiczenia 2/Zadanie 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Zadanie_1
+{
+    class Program
+    {
+            enum plec { K, M };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
