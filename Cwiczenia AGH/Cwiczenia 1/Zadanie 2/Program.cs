16ceb23c84d2b4a18112173c8806c7462a3a1143
Sugar <sugarypolecat@gmail.com>
2021-10-31 15:59:03 +0000

    moved zadanie 2 to cwiczenia 1

---
+++ Cwiczenia AGH/Cwiczenia 1/Zadanie 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadanie_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ile = 0;
+            bool toLiczba = false;
+            Console.WriteLine("Ile liczb: ");
+            while (!toLiczba)
+            {
+                toLiczba = int.TryParse(Console.ReadLine(), out ile);
+                if (!toLiczba)
+                {
+                    Console.WriteLine("Podaj ile liczb w cyfrach. Liczba musi byc calkowita.");
+                }
+
+            }
+            Console.WriteLine("Teraz podaj " + ile + " liczb ktore zssumujemy i podamy ktora jest najwieksza.");
+
+           int[] numArr = new int[ile];
+            for(int x=0; x<ile; x++)
+            {
+                int liczba = 0;
+                bool toLiczba2 = false;
+                while (!toLiczba2)
+                {
+                    Console.WriteLine("Podaj liczbe na pozycje " + x + ": ");
+                    toLiczba2 = Int32.TryParse(Console.ReadLine(), out liczba);
+                    if (!toLiczba2)
+                    {
+                        Console.WriteLine("Podaj liczbe w cyfrach. Liczba musi byc calkowita.");
+                    }
+
+                }
+                numArr[x] = Convert.ToInt32(liczba);
+            }
+            Liczby liczby = new Liczby(numArr);
+            liczby.countStuff();
+            Console.ReadLine();
+        }
+    }
+}
