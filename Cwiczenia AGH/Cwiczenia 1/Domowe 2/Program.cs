1ae52545c49fa070215896d3850895bc5cf60f6c
Sugar <sugarypolecat@gmail.com>
2021-11-01 09:59:43 +0100

    finished zadanie domow 2, i think this is what they wanted??

--- Cwiczenia AGH/Cwiczenia 1/Domowe 2/Program.cs
+++ Cwiczenia AGH/Cwiczenia 1/Domowe 2/Program.cs
@@ -6,7 +6,61 @@ namespace Domowe_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Podaj liczbe: ");
+            bool toLiczba = false;
+            uint liczba = 0;
+
+            while (!toLiczba)
+            {
+                toLiczba = uint.TryParse(Console.ReadLine(), out liczba);
+                if (!toLiczba)
+                {
+                    Console.WriteLine("Sprobuj ponownie");
+                }
+            } 
+
+                uint ilosc3 = 0;
+            try
+            {
+                uint rownanie = liczba % 3;
+            // nie wiem czy to o to chodzi.
+                if(rownanie == 0)
+                {
+                    Console.WriteLine("Liczbe mozna dzielic przez 3.");
+                }
+                else
+                {
+                    Console.WriteLine("nie, liczbny nie mozna dizlic przez 3");
+                }
+                // chyba ze o to im chodzi?
+                string liczbaString = Convert.ToString(liczba);
+                for(int x = 0; x < liczbaString.Length; x++)
+                {
+                    Console.WriteLine($"pozycja {x}: {liczbaString[x]}");
+                    // czemu to krwa double
+                    double check = Char.GetNumericValue(liczbaString[x]);
+                    if (check == 3)
+                    {
+                        Console.WriteLine("znaleziono");
+                        ilosc3++;
+                    }
+                }
+            }
+            catch (ArithmeticException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            if (ilosc3 < 1) {
+                Console.WriteLine("nie, nie znaleziono 3 w liczbnie.");
+            } else
+            {
+                Console.WriteLine($"Znaleziono {ilosc3} troijek.");
+            }
+        Console.ReadLine();
         }
     }
 }
