c1eb2389f50d57cb50599d9620cf1484f7824309
Sugar <sugarypolecat@gmail.com>
2021-11-01 11:20:28 +0100

    added skeletons for domowe 5-7

---
+++ Cwiczenia AGH/Cwiczenia 1/Domowe 7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domowe_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
