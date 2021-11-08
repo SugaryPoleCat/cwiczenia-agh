888b75c4c8dbbdc872054c24d981ca7f2add2c68
Sugar <sugarypolecat@gmail.com>
2021-11-08 08:43:12 +0100

    Domowe 5 in cwieczenia 1, is done.

--- Cwiczenia AGH/Cwiczenia 1/Domowe 5/Program.cs
+++ Cwiczenia AGH/Cwiczenia 1/Domowe 5/Program.cs
@@ -7,14 +7,25 @@ namespace Domowe_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Data kurwa urodzenia chuju");
+            Console.Write("Data twoja urodzenia (yyyyMMdd): ");
 
-            DateTime data;
+            string[] formaty = { "yyyyMMdd", "HHmmss" };
+            DateTime data = new DateTime();
             bool toData = false;
             while (!toData)
             {
-
+                toData = DateTime.TryParseExact(Console.ReadLine(), formaty, null, System.Globalization.DateTimeStyles.AllowWhiteSpaces | System.Globalization.DateTimeStyles.AdjustToUniversal, out data);
+                if (!toData)
+                {
+                    Console.WriteLine("Zle wprowadzona data.");
+                }
             }
+
+            DateTime teraz = DateTime.Now;
+            //if()
+            // YES C#!! YES I KNOW ITS EMPTY!!!! IT CAN BE FUCKING EMPTY!!!!!
+            double result = (teraz.Date - data.Date).TotalDays;
+            Console.WriteLine("Minelo: " + result + " dni od day urodzin.");
 
             Console.ReadKey();
         }
