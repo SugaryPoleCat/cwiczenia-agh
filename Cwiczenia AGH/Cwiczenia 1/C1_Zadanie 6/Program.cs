8448c5a4f670132265f94d6f4d0a02dfde92a8d1
Sugar <sugarypolecat@gmail.com>
2021-11-08 10:37:18 +0100

    Zadanie 6 is now done

--- Cwiczenia AGH/Cwiczenia 1/C1_Zadanie 6/Program.cs
+++ Cwiczenia AGH/Cwiczenia 1/C1_Zadanie 6/Program.cs
@@ -6,7 +6,30 @@ namespace C1_Zadanie_6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool isEmpty = false;
+            string tekst = "";
+
+            do
+            {
+                tekst = Console.ReadLine();
+                if (tekst == "")
+                {
+                    Console.WriteLine("Tekst nie moze byc pusty");
+                }
+                else
+                {
+                    isEmpty = true;
+                }
+            } while (!isEmpty);
+
+            string[] arrTekst = tekst.Split();
+            string result = "";
+            for (int i = arrTekst.Length; i-- > 0;)
+            {
+                result += arrTekst[i] + " ";
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
         }
     }
 }
