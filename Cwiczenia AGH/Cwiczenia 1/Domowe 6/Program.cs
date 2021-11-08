828cfc6f0e65f89de1064a68a4743938ae84d76c
Sugar <sugarypolecat@gmail.com>
2021-11-08 08:46:09 +0100

    Domowe 6 is -- a little too easy?

--- Cwiczenia AGH/Cwiczenia 1/Domowe 6/Program.cs
+++ Cwiczenia AGH/Cwiczenia 1/Domowe 6/Program.cs
@@ -6,7 +6,13 @@ namespace Domowe_6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DateTime data = new DateTime(2021, 12, 31, 23, 59, 59);
+            DateTime teraz = DateTime.Now;
+
+            double result = (data.Date - teraz.Date).TotalDays;
+            Console.WriteLine("Pozosalo: " + result + " dni do konca roku.");
+
+            Console.ReadKey();
         }
     }
 }
