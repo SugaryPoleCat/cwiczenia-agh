4f83451abe63f1029942d5e9d74f02fe82818196
Sugar <sugarypolecat@gmail.com>
2021-11-08 10:21:41 +0100

    added skeletons to zadania 1, 3, 4, 5, 6

---
+++ Cwiczenia AGH/Cwiczenia 1/C1_Zadanie 3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C1_Zadanie_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Cwiczenia AGH/Cwiczenia 1/C1_Zadanie 4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C1_Zadanie_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
